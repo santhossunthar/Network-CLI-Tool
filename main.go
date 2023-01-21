f0c13c3a8e3247c27c5dcfcdd4a4814d73faa6d8
santhossunthar <santhoshsunthar@gmail.com>
2023-01-21 23:21:05 +0530

    Added name servers look up

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Network CLI Tool"
+
+	myFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "example.com",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "nslookup",
+			Usage: "Look up the name servers",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil {
+					return err
+				}
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
