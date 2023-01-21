c3edd0ce408ad32c5ec12622b483879d758f8b54
santhossunthar <santhoshsunthar@gmail.com>
2023-01-21 23:48:52 +0530

    Added mx records look up

--- main.go
+++ main.go
@@ -30,9 +30,11 @@ func main() {
 				if err != nil {
 					return err
 				}
+
 				for i := 0; i < len(ns); i++ {
 					fmt.Println(ns[i].Host)
 				}
+				
 				return nil
 			},
 		},
@@ -43,7 +45,7 @@ func main() {
 			Action: func(c *cli.Context) error {
 				cnames, err := net.LookupCNAME(c.String("host"))
 				if err != nil {
-					return nil
+					return err
 				}
 
 				fmt.Println(cnames)
@@ -57,11 +59,28 @@ func main() {
 			Action: func (c *cli.Context) error  {
 				ips, err := net.LookupIP(c.String("host"))
 				if err != nil {
-					return nil
+					return err
 				}
 
 				for i := 0; i < len(ips); i++ {
 					fmt.Println(ips[i])
+				}
+
+				return nil
+			},
+		},
+		{
+			Name: "mxlookup",
+			Usage: "Look up the MX records",
+			Flags: myFlags,
+			Action: func (c *cli.Context) error  {
+				mxs, err := net.LookupMX(c.String("host"))
+				if err != nil {
+					return err
+				}
+
+				for i := 0; i < len(mxs); i++ {
+					fmt.Println(mxs[i].Host, mxs[i].Pref)
 				}
 
 				return nil
